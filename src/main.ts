28dccb85e946cd06a805cd9805c24080565c7013
Unfield <gpgvip@gmail.com>
2023-04-12 05:36:10 +0200

    added pino for logging

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import yaml from "js-yaml";
 import { readFileSync } from "node:fs";
 import { ConfigSchema, type ConfigType } from "./types";
+import { pino } from "pino";
 
+const logger = pino();
+logger.info("Starting Service...");
 let config: ConfigType = loadConfig();
 
 function loadConfig() {
   try {
-    return ConfigSchema.parse(yaml.load(readFileSync("config.yaml", "utf-8")));
+    let loadedConfig = ConfigSchema.parse(
+      yaml.load(readFileSync("config.yaml", "utf-8"))
+    );
+    logger.info("Configuration loaded successfully.");
+    return loadedConfig;
   } catch (e) {
-    console.error("Failed to load config.yaml!");
+    logger.fatal("Failed to load config.yaml!");
     process.exit(1);
   }
 }
